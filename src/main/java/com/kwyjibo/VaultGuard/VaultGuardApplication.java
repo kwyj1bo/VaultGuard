d14c5d8da2d72dca26b2dd7f32ca6ffa444e42b3
kwyj1bo <pratikjlande@gmail.com>
2026-03-03 21:25:02 +0530

    first

---
+++ src/main/java/com/kwyjibo/VaultGuard/VaultGuardApplication.java
@@ -0,0 +1,13 @@
+package com.kwyjibo.VaultGuard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultGuardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kwyjibo/VaultGuard/VaultGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwyjibo.VaultGuard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
