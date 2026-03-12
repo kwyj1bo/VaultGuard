7d206674c60fbca028bc5cf4faee70d372906d3a
kwyj1bo <pratikjlande@gmail.com>
2026-03-12 13:11:53 +0530

    JitRepo done

---
+++ src/main/java/com/kwyjibo/VaultGuard/repository/JitRequestRepository.java
@@ -0,0 +1,11 @@
+package com.kwyjibo.VaultGuard.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kwyjibo.VaultGuard.model.JitRequest;
+
+@Repository
+public interface JitRequestRepository extends JpaRepository<JitRequest, Long> {
+
+}
